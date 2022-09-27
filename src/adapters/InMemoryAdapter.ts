931314a7fc7366c54d885ec0ad3c0c0ccfc02c82
kluivert.queiroz <kluivert.queiroz@hotmail.com>
2022-09-27 20:01:31 -0300

    simplify code

--- src/adapters/InMemoryAdapter.ts
+++ src/adapters/InMemoryAdapter.ts
@@ -2,11 +2,11 @@ import Message from "../message/Message";
 import Adapter from "./Adapter";
 export default class InMemoryAdapter implements Adapter {
   readonly messages = [];
-  save(message: Message): Promise<boolean> {
+  async save(message: Message): Promise<boolean> {
     this.messages.push(message);
-    return Promise.resolve(true);
+    return true;
   }
-  poll(): Promise<Message | null> {
-    return Promise.resolve(this.messages.shift());
+  async poll(): Promise<Message | null> {
+    return this.messages.shift();
   }
 }
